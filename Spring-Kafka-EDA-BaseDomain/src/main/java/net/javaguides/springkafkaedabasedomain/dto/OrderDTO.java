1a47149b13c60388dcf71ca55e8ab87187474c4b
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-07-01 12:26:14 -0300

    Improving OrderDTO

--- Spring-Kafka-EDA-BaseDomain/src/main/java/net/javaguides/springkafkaedabasedomain/dto/OrderDTO.java
+++ Spring-Kafka-EDA-BaseDomain/src/main/java/net/javaguides/springkafkaedabasedomain/dto/OrderDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data @AllArgsConstructor @NoArgsConstructor
 public class OrderDTO {
 
-    private Long id;
+    private String orderId;
 
     private String name;
 
